b508f50871b686eb8243a2b39fad8a50669ab655
8208304124 <94159089+8208304124@users.noreply.github.com>
2021-11-18 12:59:43 +0530

    Update index.js

--- index.js
+++ index.js
@@ -17,6 +17,8 @@ if (leadsFromLocalStorage1 && leadsFromLocalStorage2 && leadsFromLocalStorage3) 
 }
 
 function add(){
+     if(input.value ==="" && idate.value==="" && inumber.value==="")
+    return alert("Please Enter Details");
 names.push(input.value);
 date.push(idate.value);
 amount.push(inumber.value);
